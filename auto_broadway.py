d52e6a065ae98fe13e8f89fa5233c9d6336a7806
phildong <35715936+phildong@users.noreply.github.com>
2023-08-18 14:46:13 -0400

    implement entering all available lotteries

--- auto_broadway.py
+++ auto_broadway.py
@@ -6,7 +6,10 @@ import time
 
 import undetected_chromedriver as uc
 import yaml
-from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import (
+    ElementNotInteractableException,
+    NoSuchElementException,
+)
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.select import Select
 
@@ -41,32 +44,42 @@ for cur_user, info in user_dict.items():
         time.sleep(random.random() * 10)  # sleep 0-10 sec
     driver.get(URL)
     try:
-        lottery_button = driver.find_element(By.CLASS_NAME, "enter-lottery-link")
+        lottery_buttons = driver.find_elements(By.CLASS_NAME, "enter-lottery-link")
     except NoSuchElementException:
         logging.error("No lottery opening at now, closing.")
         break
-    lottery_button.click()
-    iframe = driver.find_element(By.CLASS_NAME, "fancybox-iframe")
-    driver.switch_to.frame(iframe)
-    driver.find_element(By.NAME, "dlslot_name_first").send_keys(info["first_name"])
-    driver.find_element(By.NAME, "dlslot_name_last").send_keys(info["last_name"])
-    qty = driver.find_element(By.NAME, "dlslot_ticket_qty")
-    Select(qty).select_by_visible_text("2")
-    driver.find_element(By.NAME, "dlslot_email").send_keys(info["email"])
-    driver.find_element(By.NAME, "dlslot_dob_year").send_keys(info["dob"]["yy"])
-    driver.find_element(By.NAME, "dlslot_dob_month").send_keys(info["dob"]["mm"])
-    driver.find_element(By.NAME, "dlslot_dob_day").send_keys(info["dob"]["dd"])
-    driver.find_element(By.NAME, "dlslot_zip").send_keys(info["zip"])
-    country = driver.find_element(By.NAME, "dlslot_country")
-    Select(country).select_by_visible_text("USA")
-    driver.execute_script("document.getElementById('dlslot_agree').click()")
-    driver.find_element(By.CSS_SELECTOR, ".enter-now-button").click()
-    result = driver.find_element(By.CLASS_NAME, "entry-header")
-    # reason = driver.find_element(By.CLASS_NAME, "entry-content").find_element(
-    #     By.TAG_NAME, "h3"
-    # )
-    if result.text == "SUCCESS":
-        logging.info("Lottery entered for {}".format(cur_user))
-    else:
-        logging.error("Unsucessful lottery for {}: {}".format(cur_user, result.text))
+    for ilottery in range(len(lottery_buttons)):
+        driver.get(URL)
+        lottery_button = driver.find_elements(By.CLASS_NAME, "enter-lottery-link")[
+            ilottery
+        ]
+        try:
+            lottery_button.click()
+        except ElementNotInteractableException:
+            continue
+        iframe = driver.find_element(By.CLASS_NAME, "fancybox-iframe")
+        driver.switch_to.frame(iframe)
+        driver.find_element(By.NAME, "dlslot_name_first").send_keys(info["first_name"])
+        driver.find_element(By.NAME, "dlslot_name_last").send_keys(info["last_name"])
+        qty = driver.find_element(By.NAME, "dlslot_ticket_qty")
+        Select(qty).select_by_visible_text("2")
+        driver.find_element(By.NAME, "dlslot_email").send_keys(info["email"])
+        driver.find_element(By.NAME, "dlslot_dob_year").send_keys(info["dob"]["yy"])
+        driver.find_element(By.NAME, "dlslot_dob_month").send_keys(info["dob"]["mm"])
+        driver.find_element(By.NAME, "dlslot_dob_day").send_keys(info["dob"]["dd"])
+        driver.find_element(By.NAME, "dlslot_zip").send_keys(info["zip"])
+        country = driver.find_element(By.NAME, "dlslot_country")
+        Select(country).select_by_visible_text("USA")
+        driver.execute_script("document.getElementById('dlslot_agree').click()")
+        driver.find_element(By.CSS_SELECTOR, ".enter-now-button").click()
+        result = driver.find_element(By.CLASS_NAME, "entry-header")
+        # reason = driver.find_element(By.CLASS_NAME, "entry-content").find_element(
+        #     By.TAG_NAME, "h3"
+        # )
+        if result.text == "SUCCESS":
+            logging.info("Lottery entered for {}".format(cur_user))
+        else:
+            logging.error(
+                "Unsucessful lottery for {}: {}".format(cur_user, result.text)
+            )
 driver.close()
